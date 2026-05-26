abdb1fb9346875ac31c00cc10fef86465056002b
wade <wadechen@outlook.com>
2026-05-26 12:38:43 +0800

    update

--- mainyan_daily_task.bat
+++ mainyan_daily_task.bat
@@ -3,8 +3,8 @@
 set "SCRIPT=C:\mainyan-auto\mainyan_daily_task.py"
 set "PYTHON=%LOCALAPPDATA%\Programs\Python\Python311\python.exe"
 
-echo PYTHON=%PYTHON% >> "%~dp0mainyan_daily_task.log"
-echo SCRIPT=%SCRIPT% >> "%~dp0mainyan_daily_task.log"
+echo PYTHON=%PYTHON% >> "%~dp0log\mainyan_daily_task.log"
+echo SCRIPT=%SCRIPT% >> "%~dp0log\mainyan_daily_task.log"
 
 "%PYTHON%" "%SCRIPT%"
 
