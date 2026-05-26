d80a59bd0709807c543fcfbec98f1710d84d4ffe
wade <wadechen@outlook.com>
2026-05-26 11:53:07 +0800

    update

--- mainyan_daily_task.py
+++ mainyan_daily_task.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 
 def run_task(days):
-    subprocess.run(["python", "product_request_export.py", "--headless", f"--days={days}"])
+    subprocess.run(["python", r"C:\mainyan-auto\product_request_export.py", "--headless", f"--days={days}"])
 
 schedule.every().day.at("07:50").do(run_task, days=1)
 schedule.every().day.at("12:50").do(run_task, days=2)
