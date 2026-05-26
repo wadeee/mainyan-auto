f16bb921c76ae23ff1e50b2b2a974a4366201a8b
wade <wadechen@outlook.com>
2026-05-26 09:22:17 +0800

    update

--- daily_task.bat
+++ daily_task.bat
@@ -1,16 +1,18 @@
 @echo off
 
-set SCRIPT=D:\project\product_request_export.py
+set SCRIPT=C:\Users\Wadec\Documents\projects\mainyan-autoproduct_request_export.py
 
 schtasks /create /f ^
-/tn "ProductExport_Days1" ^
-/tr python \"%SCRIPT%\" --headless --days 1" ^
-/sc daily /st 08:00
+  /tn "ProductExport_Days1" ^
+  /tr python \"%SCRIPT%\" --headless --days 1" ^
+  /sc daily /st 08:00 ^
+  /ru SYSTEM
 
 schtasks /create /f ^
-/tn "ProductExport_Days2" ^
-/tr python \"%SCRIPT%\" --headless --days 2" ^
-/sc daily /st 13:00
+  /tn "ProductExport_Days2" ^
+  /tr python \"%SCRIPT%\" --headless --days 2" ^
+  /sc daily /st 13:00 ^
+  /ru SYSTEM
 
 echo Done
 pause
