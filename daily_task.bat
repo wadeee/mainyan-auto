704e1f961e892b1b7ab353c699bf33591f870966
wade <wadechen@outlook.com>
2026-05-26 10:00:51 +0800

    update

--- daily_task.bat
+++ daily_task.bat
@@ -5,7 +5,7 @@ set SCRIPT=%~dp0product_request_export.py
 
 schtasks /create /f /tn "ProductExport_Days1" /tr "\"%PYTHON%\" \"%SCRIPT%\" --headless --days 1" /sc daily /st 07:50 /ru SYSTEM
 
-schtasks /create /f /tn "ProductExport_Days2" /tr ""\"%PYTHON%\" \"%SCRIPT%\" --headless --days 2" /sc daily /st 12:50 /ru SYSTEM
+schtasks /create /f /tn "ProductExport_Days2" /tr "\"%PYTHON%\" \"%SCRIPT%\" --headless --days 2" /sc daily /st 12:50 /ru SYSTEM
 
 echo Done
 pause
