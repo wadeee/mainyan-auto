62bc7dc44278d571d749774d2424ed05e27b4b60
wade <wadechen@outlook.com>
2026-05-26 12:39:11 +0800

    update

--- mainyan_daily_task.bat
+++ mainyan_daily_task.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set "SCRIPT=C:\mainyan-auto\mainyan_daily_task.py"
-set "PYTHON=%LOCALAPPDATA%\Programs\Python\Python311\python.exe"
+set "PYTHON=C:\Users\Wadec\AppData\Local\Programs\Python\Python311\python.exe"
 
 echo PYTHON=%PYTHON% >> "%~dp0log\mainyan_daily_task.log"
 echo SCRIPT=%SCRIPT% >> "%~dp0log\mainyan_daily_task.log"
