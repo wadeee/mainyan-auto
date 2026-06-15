22a80f096de01ba40a84932d30d72c6b5fc25261
wade <wadechen@outlook.com>
2026-06-15 14:21:50 +0800

    update

--- finance/Chrome-美团经营宝-招行后台-抖音后台.bat
+++ finance/Chrome-美团经营宝-招行后台-抖音后台.bat
@@ -1,2 +1,9 @@
 @echo off
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9226 --user-data-dir="C:\ChromeDebug_MTJYB"
+
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" ^
+    --remote-debugging-port=9226 ^
+    --user-data-dir="C:\ChromeDebug_MTJYB" ^
+    --new-window ^
+    "https://ecom.meituan.com/meishi" ^
+    "https://ym.o2o.cmbchina.com/mc/merchant/handms/login.html" ^
+    "https://life.douyin.com/p/login"
